28f12878c1a3947dedb448edec5837c9e1962adc
Sydney <sstalker@csumb.edu>
2025-04-03 17:07:54 -0700

    End of video 5.

--- app/src/main/java/com/cst338/gymlog/MainActivity.java
+++ app/src/main/java/com/cst338/gymlog/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        repository = new GymLogRepository(getApplication());
+        repository = GymLogRepository.getRepository(getApplication());
 
         binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
 
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "current info" + currentInfo);
         String newDisplay = String.format(Locale.US,"Exercise:%s%nWeight:%.2f%nReps:%d%n=-=-=-=%n%s",mExercise,mWeight,mReps, currentInfo);
         binding.logDisplayTextView.setText(newDisplay);
+        Log.i(TAG, repository.getAllLogs().toString());
     }
 
 
