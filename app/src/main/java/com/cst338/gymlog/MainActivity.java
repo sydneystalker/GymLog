a8b75d21435afd628a0eb8f6c3dcd44164b1a037
Sydney <sstalker@csumb.edu>
2025-04-06 17:46:50 -0700

    End of video 8.

--- app/src/main/java/com/cst338/gymlog/MainActivity.java
+++ app/src/main/java/com/cst338/gymlog/MainActivity.java
@@ -1,5 +1,7 @@
 package com.cst338.gymlog;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -15,6 +17,7 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final String MAIN_ACTIVITY_USER_ID = "com.cst338.gymlog.MAIN_ACTIVITY_USER_ID";
     private ActivityMainBinding binding;
     private GymLogRepository repository;
     public static final String TAG = "SS_GYMLOG";
@@ -30,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        loginUser();
+
+        if(loggedInUserId == -1){
+            Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
+            startActivity(intent);
+        }
 
         repository = GymLogRepository.getRepository(getApplication());
 
@@ -52,7 +62,18 @@ public class MainActivity extends AppCompatActivity {
          }
      });
     }
-    
+
+    private void loginUser() {
+        //TODO: create login method
+        loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, -1);
+    }
+
+    static Intent mainActivityIntentFactory(Context context, int userId){
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
+        return intent;
+    }
+
     private void insertGymLogRecord(){
         if(mExercise.isEmpty()){
             return;
