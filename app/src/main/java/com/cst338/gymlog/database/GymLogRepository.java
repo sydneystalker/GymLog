3cbe363446abacf9152c961c6fedc332601b71c0
Sydney <sstalker@csumb.edu>
2025-04-07 15:25:58 -0700

    End of video 10.

--- app/src/main/java/com/cst338/gymlog/database/GymLogRepository.java
+++ app/src/main/java/com/cst338/gymlog/database/GymLogRepository.java
@@ -79,4 +79,19 @@ public class GymLogRepository {
         });
     }
 
+    public User getUserByUsername(String username) {
+        Future<User> future = GymLogDatabase.databaseWriteExecutor.submit(
+                new Callable<User>() {
+                    @Override
+                    public User call() throws Exception {
+                        return userDAO.getUserByUsername(username);
+                    }
+                });
+        try{
+            future.get();
+        } catch (InterruptedException | ExecutionException e){
+            Log.i(MainActivity.TAG, "Problem when getting user by username");
+        }
+        return null;
+    }
 }
